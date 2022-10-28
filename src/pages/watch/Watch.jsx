f00cac24dd8109a9316be1d4b7fc924713a308bf
Fahad Ali <fahad.ali@smodin.io>
2022-10-28 20:06:59 +0500

    fix unused var

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -1,5 +1,5 @@
 import { ArrowBackOutlined } from "@material-ui/icons";
-import { Link, useLocation } from "react-router-dom";
+import { Link} from "react-router-dom";
 import "./watch.scss";
 
 export default function Watch() {
