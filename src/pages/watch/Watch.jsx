73d69d2c332787f6590b73f408c3793168df1f2b
Fahad Ali <fahad.ali@smodin.io>
2022-10-28 19:57:41 +0500

    fix unused var

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -3,8 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import "./watch.scss";
 
 export default function Watch() {
-  const location = useLocation();
-  // const movie = location.movie;
+
   return (
     <div className="watch">
       <Link to="/">
