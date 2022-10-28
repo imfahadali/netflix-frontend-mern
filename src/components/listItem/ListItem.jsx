e7559a646d1a7ed41e22ef95dfc9061d311b7e08
Fahad Ali <fahad.ali@smodin.io>
2022-10-28 18:44:56 +0500

    first commit

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,75 @@
+import "./listItem.scss";
+import {
+  PlayArrow,
+  Add,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function ListItem({ index, item }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [movie, setMovie] = useState({});
+  const movieDuration = movie?.duration ?? "102min";
+
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const res = await axios.get("/movies/find/" + item, {
+          headers: {
+            token:
+              "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+          },
+        });
+        setMovie(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMovie();
+  }, [item]);
+
+  return (
+    <Link to={{ pathname: "/watch", movie: movie }}>
+      <div
+        className="listItem"
+        style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img src={movie?.imgSm} alt="" />
+        {isHovered && (
+          <>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/_Vw3Nur4YDo?autoplay=1&mute=1&loop=1"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+              className={isHovered && "visible"}
+            ></iframe>
+            <div className="itemInfo">
+              <div className="icons">
+                <PlayArrow className="icon" />
+                <Add className="icon" />
+                <ThumbUpAltOutlined className="icon" />
+                <ThumbDownOutlined className="icon" />
+              </div>
+              <div className="itemInfoTop">
+                <span>{movieDuration}</span>
+                <span className="limit">+{movie?.limit}</span>
+                <span>{movie?.year}</span>
+              </div>
+              <div className="desc">{movie?.desc.substring(0, 140)}</div>
+              <div className="genre">{movie?.genre}</div>
+            </div>
+          </>
+        )}
+      </div>
+    </Link>
+  );
+}
---
+++ src/pages/watch/Watch.jsx
@@ -0,0 +1,27 @@
+import { ArrowBackOutlined } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
+import "./watch.scss";
+
+export default function Watch() {
+  const location = useLocation();
+  const movie = location.movie;
+  return (
+    <div className="watch">
+      <Link to="/">
+        <div className="back">
+          <ArrowBackOutlined />
+          Home
+        </div>
+      </Link>
+      <iframe
+        width="99%"
+        height="99%"
+        src="https://www.youtube.com/embed/_Vw3Nur4YDo?autoplay=1&mute=1&loop=1&modestbranding=1&showinfo=0&rel=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
