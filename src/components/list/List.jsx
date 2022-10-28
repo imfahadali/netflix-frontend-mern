cfaf2acc1fa3b4349460ea32f39b8544c52e1590
Fahad Ali <fahad.ali@smodin.io>
2022-10-28 19:52:05 +0500

    remove unused vars netlify error

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -9,7 +9,7 @@ import "./list.scss";
 export default function List({ list }) {
   const [isMoved, setIsMoved] = useState(false);
   const [slideNumber, setSlideNumber] = useState(0);
-  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
+  const [clickLimit] = useState(window.innerWidth / 230);
 
   const listRef = useRef();
 
