21f7f9eded71510db5080ff6b46e535671a003b9
Fahad Ali <fahad.ali@smodin.io>
2022-10-29 13:04:11 +0500

    fix setState delay on password and username

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,28 +1,29 @@
 import axios from "axios";
-import { useRef } from "react";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./register.scss";
 
 export default function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState(false);
+  const [credentials, setCredentials] = useState({});
   const history = useHistory();
 
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const usernameRef = useRef();
-
-  const handleStart = () => {
-    setEmail(emailRef.current.value);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
   };
   const handleFinish = async (e) => {
     e.preventDefault();
-    setPassword(passwordRef.current.value);
-    setUsername(usernameRef.current.value);
+
     try {
-      await axios.post("auth/register", { email, username, password });
+      await axios.post("auth/register", {
+        email: credentials.email,
+        username: credentials.username,
+        password: credentials.password,
+      });
       history.push("/login");
     } catch (err) {
       console.log(err);
@@ -53,15 +54,30 @@ export default function Register() {
         </p>
         {!email ? (
           <div className="input">
-            <input type="email" placeholder="email address" ref={emailRef} />
-            <button className="registerButton" onClick={handleStart}>
+            <input
+              type="email"
+              placeholder="email address"
+              name="email"
+              onChange={handleChange}
+            />
+            <button className="registerButton" onClick={()=> setEmail(true)}>
               Get Started
             </button>
           </div>
         ) : (
           <form className="input">
-            <input type="username" placeholder="username" ref={usernameRef} />
-            <input type="password" placeholder="password" ref={passwordRef} />
+            <input
+              type="username"
+              placeholder="username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              onChange={handleChange}
+            />
             <button className="registerButton" onClick={handleFinish}>
               Start
             </button>
